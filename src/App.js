5f19ca92c3ea50bcd1070eb1e1c4adda1de23630
wwwbrunagon <wwwbrunagon@gmail.com>
2020-05-12 17:49:01 -0300

    implemented the method of translating

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
+function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
+  return (
+    <div className="App">
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
+        <button onClick={()=>handleClick('pt')} >
+            Portugues
+          </button>
+          <button onClick={()=>handleClick('en')} >
+            English
+          </button>   
+        </nav>
+        <header className="App-header">
+          
+          <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3> 
+          
+        </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import px2vw from "../utils/px2vw";
+
+export const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+  :root {
+      font-size: ${px2vw(24)};
+
+      @media (min-width: 768px) {
+        font-size: ${px2vw(18)};
+      }
+
+      @media (min-width: 1024px) {
+        font-size: ${px2vw(16)};
+      }
+    }
+  
+    
+`;
+
+export default Global;
